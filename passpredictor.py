f3100d2d2970df753979cf42aa1cf5137b0216ae
Sam Friedman <samtx@outlook.com>
2019-07-24 10:25:49 -0700

    add rv2coe and tests

--- passpredictor.py
+++ passpredictor.py
@@ -1,26 +1,25 @@
 import numpy as np
-from numpy import dot
+from numpy import dot, cross
 from math import sqrt, sin, cos, asin, acos, radians, degrees, pi
 from pprint import pprint
 
 # Constants
-R_EARTH  = np.float64(6378.137) # [km] Mean equatorial radius
-R2_EARTH = np.longdouble(40680631.59076899)  # [km2] mean equatorial radius squared
-e_EARTH  = np.longdouble(0.081816221456)  # Earth eccentricity
-e2_EARTH = np.longdouble(0.006694385000)  # Earth eccentricity squared
+R_EARTH = np.float64(6378.137)               # [km] Mean equatorial radius
+R2_EARTH = np.longdouble(40680631.59076899)  # [km2] mean equat. radius sq.
+e_EARTH = np.longdouble(0.081816221456)      # Earth eccentricity
+e2_EARTH = np.longdouble(0.006694385000)     # Earth eccentricity squared
 MU = np.longdouble(398600.4418)  # [km3/(solar s)2] gravitational parameter
 J2 = np.longdouble(0.0010826267)
 
 
 def site_declination_and_K(phi_gd, h_ellp):
     """
-    Vallado, Eq.3-7 
+    Vallado, Eq.3-7
     Get declination and K vectors for site on Earth
     Note: currently only precise to 0.1 km
     """
-    # Eq.(3-7)
     phi_gd_rad = radians(np.float64(phi_gd))  # convert [deg] to [rad]
-    h_ellp_km = np.float64(h_ellp)*(1/1000)      # convert [m] to [km]
+    h_ellp_km = np.float64(h_ellp)*(1/1000)   # convert [m] to [km]
     C = R_EARTH / np.sqrt(1 - e2_EARTH*sin(phi_gd_rad)**2)
     S = C*(1-e2_EARTH)
     r_delta = (C + h_ellp_km)*cos(phi_gd_rad)
@@ -47,9 +46,9 @@ def site_ECEF(phi_gd, lmda, h_ellp):
     return r_site_ECEF
 
 
-
 def fk5():
     pass
+
 
 def nu2anomaly(nu, e):
     """
@@ -63,6 +62,7 @@ def nu2anomaly(nu, e):
         E = acos(cosE)
     return degrees(E)
 
+
 def anomaly2nu(e, E):
     """
     Vallado, alg 6, p.77
@@ -73,15 +73,16 @@ def anomaly2nu(e, E):
         nu = acos(cosNu)
     return degrees(nu)
 
+
 def rot1(a):
     """
     Euler angle rotation matrix, first angle
     Vallado, Eq.3-15
     """
     mtx = np.array([
-        [  1.,     0.,     0.],
-        [  0., cos(a), sin(a)],
-        [  0.,-sin(a), cos(a)]
+        [1.,      0.,     0.],
+        [0.,  cos(a), sin(a)],
+        [0., -sin(a), cos(a)]
     ])
     return mtx
 
@@ -92,9 +93,9 @@ def rot2(a):
     Vallado, Eq.3-15
     """
     mtx = np.array([
-        [cos(a),     0.,-sin(a)],
-        [    0., cos(a),     0.],
-        [sin(a),     0., cos(a)]
+        [cos(a),     0., -sin(a)],
+        [0.,     cos(a),      0.],
+        [sin(a),     0.,  cos(a)]
     ])
     return mtx
 
@@ -105,14 +106,12 @@ def rot3(a):
     Vallado, Eq.3-15
     """
     mtx = np.array([
-        [ cos(a), sin(a),   0.],
-        [-sin(a), cos(a),   0.],
-        [     0.,  cos(a),  1.]
+        [cos(a), sin(a),   0.],
+        [-sin(a), cos(a),  0.],
+        [0.,      cos(a),  1.]
     ])
     return mtx
 
-def rot313(a,b,c):
-    return dot(dot(rot3(a),rot1(b)),rot3(c))
 
 def coe2rv(p, e, i, Omega, w, nu, u=0., lmda_true=0., w_hat_true=0.):
     """
@@ -150,14 +149,9 @@ def coe2rv(p, e, i, Omega, w, nu, u=0., lmda_true=0., w_hat_true=0.):
         sqrt_MU_p*(e + cos_nu),
         0.
     ])
-    
-    pprint(rPQW)
-    pprint(vPQW)
-    
+
     Omega_rad = radians(Omega)
     w_rad = radians(w)
-
-    # rot_mtx = rot3(-Omega_rad).dot(rot1(-i_rad).dot(rot3(-w_rad)))
     cos_Om = cos(Omega_rad)
     cos_w = cos(w_rad)
     cos_i = cos(i_rad)
@@ -170,13 +164,85 @@ def coe2rv(p, e, i, Omega, w, nu, u=0., lmda_true=0., w_hat_true=0.):
         [sin_Om*cos_w+cos_Om*sin_w*cos_i, -sin_Om*sin_w+cos_Om*cos_w*cos_i, -cos_Om*sin_i],
         [                    sin_w*sin_i,                      cos_w*sin_i,         cos_i]
     ])
-    pprint(rot_mtx)
 
-    rIJK = dot(rot_mtx,rPQW)
-    vIJK = dot(rot_mtx,vPQW)
+    rIJK = dot(rot_mtx, rPQW)
+    vIJK = dot(rot_mtx, vPQW)
 
     return rIJK, vIJK
 
+
+def rv2coe(rIJK, vIJK, findall=False):
+    """
+    Vallado, Alg 9, p.113
+    """
+    hvec = cross(rIJK, vIJK)
+    h = sqrt(dot(hvec, hvec))
+    Khat = np.array([0., 0., 1.])
+    nvec = cross(Khat, hvec)
+    rmag = sqrt(dot(rIJK, rIJK))
+    v2 = dot(vIJK, vIJK)
+    rmaginv = 1.0/rmag
+    evec = (1/MU)*((v2 - MU*rmaginv)*rIJK - dot(rIJK, vIJK)*vIJK)
+    e = sqrt(dot(evec, evec))
+
+    xi = v2*0.5 - MU*rmaginv
+
+    if e != 1.0:
+        a = -MU/(2*xi)
+        p = a*(1 - e**2)
+    else:
+        p = h**2/MU
+        p = np.inf
+
+    cos_i = hvec[2]/h
+    i = degrees(acos(cos_i))
+
+    n = sqrt(dot(nvec, nvec))
+    cos_Om = nvec[0]/n
+    cos_w = dot(nvec, evec)/(n*e)
+    cos_nu = dot(evec, rIJK)*rmaginv*(1/e)
+
+    if nvec[1] < 0.:
+        Omega = 360 - degrees(acos(cos_Om))
+    else:
+        Omega = degrees(acos(cos_Om))
+
+    if evec[2] < 0.:
+        w = 360 - degrees(acos(cos_w))
+    else:
+        w = degrees(acos(cos_w))
+
+    if dot(rIJK, vIJK) < 0.:
+        nu = 360 - degrees(acos(cos_nu))
+    else:
+        nu = degrees(acos(cos_nu))
+
+    # special cases
+    small = 1e-10
+    what_true, lmda_true, u = 0., 0., 0.
+    # elliptical equatorial
+    if ((i < small or abs(pi - i) < small) and (e >= small)) or findall:
+        cos_what_true = evec[0]/e
+        if evec[1] < 0:
+            what_true = 360 - degrees(acos(cos_what_true))
+        else:
+            what_true = degrees(acos(cos_what_true))
+    # circular inclined
+    if ((i >= small or abs(pi - i) >= small) and (e < small)) or findall:
+        cos_u = dot(nvec, rIJK)*(1/n)*rmaginv
+        if rIJK[2] < 0:
+            u = 360 - degrees(acos(cos_u))
+        else:
+            u = degrees(acos(cos_u))
+    # circular equatorial
+    if ((i < small or abs(pi - i) < small) and (e < small)) or findall:
+        cos_lmda_true = rIJK[0]*rmaginv
+        if rIJK[1] < 0:
+            lmda_true = 360 - degrees(acos(cos_lmda_true))
+        else:
+            lmda_true = degrees(acos(cos_lmda_true))
+
+    return (p, a, e, i, Omega, w, nu, u, lmda_true, what_true)
 
 
 def kepEqtnE(M, e):
@@ -224,9 +290,13 @@ def pkepler_coe(a0, e0, i0, Omega0, w0, nu0, u, lmda_true, what_true, dt, ndt=0.
     E = kepEqtnE(M, e)
     if e != 0.0:
         nu = anomaly2nu(E, e)
+        u, lmda_true = 0., 0.
     else:
+        nu = nu0
         u, lmda_true = E, E
 
+    r, v = coe2rv(p, e, i0, Omega, w, nu, u, lmda_true)
+    return r, v
 
 def sgp4():
     pass
