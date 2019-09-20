51caa339b776c14ded3e0126de10975dc6f450c6
Sam Friedman <samtx@outlook.com>
2019-09-19 20:57:48 -0600

    Separate the coe and pkepler functions into astro.py

--- test_passpredictor.py
+++ test_passpredictor.py
@@ -5,6 +5,7 @@ from numpy.testing import assert_allclose, assert_almost_equal
 import numpy as np
 from datetime import datetime, timedelta
 
+
 def test_site_declination_and_K():
     """
     Vallado, Eg 3-1
@@ -17,6 +18,7 @@ def test_site_declination_and_K():
     assert_almost_equal(rdelta_calc, rdelta_true, decimal=3)
     assert_almost_equal(rK_calc, rK_true, decimal=3)
 
+
 def test_site_declination_and_K_2():
     """
     Vallado, Eg 7-1, p.431
@@ -28,6 +30,7 @@ def test_site_declination_and_K_2():
     assert_almost_equal(rdelta_calc, rdelta_true, decimal=2)
     assert_almost_equal(rK_calc, rK_true, decimal=2)
 
+
 def test_site_ECEF():
     """
     Vallado, Eg 7-1, p.431
@@ -38,128 +41,6 @@ def test_site_ECEF():
     r_ECEF = pp.site_ECEF(phi_gd, lmda, alt)
     r_ECEFtrue = np.array([-1275.1219, -4797.9890, 3994.2975])
     assert_allclose(r_ECEF, r_ECEFtrue)
-
-
-def test_coe2rv():
-    """
-    Vallado, Eg.2-6, p.116
-    """
-    p = 11067.790   # [km]
-    e = 0.83285
-    i = 87.87       # [deg]
-    Omega = 227.89  # [deg]
-    w = 53.38       # [deg]
-    nu = 92.335     # [deg]
-    r_calc, v_calc = pp.coe2rv(p, e, i, Omega, w, nu)
-    r_true = np.array([6525.344, 6861.535, 6449.125])  # [km]
-    v_true = np.array([4.902276, 5.533124, -1.975709]) # [km/s]
-    assert_allclose(r_calc, r_true, atol=1e-1)
-    assert_allclose(v_calc, v_true, rtol=1e-5)
-
-
-def test_rv2coe():
-    """
-    Vallado, Eg.2-5, p.114
-    """
-    rIJK = np.array([6524.834, 6862.875, 6448.296]) # [km]
-    vIJK = np.array([4.901327, 5.533756, -1.976341]) # [km/s]
-    p_true, a_true, e_true = 11067.790, 36127.343, 0.832853
-    i_true, Omega_true, w_true = 87.870, 227.898, 53.38
-    nu_true, u_true, lmda_true_true, what_true_true = 92.335, 145.60549, 55.282587, 247.806
-    coe = pp.rv2coe(rIJK, vIJK, findall=True)
-    assert_almost_equal(coe['p'], p_true, decimal=2)
-    assert_almost_equal(coe['a'], a_true, decimal=2)
-    assert_almost_equal(coe['e'], e_true, decimal=6)
-    assert_almost_equal(coe['i'], i_true, decimal=3)
-    assert_almost_equal(coe['Omega'], Omega_true, decimal=3)
-    assert_almost_equal(coe['w'], w_true, decimal=2)
-    assert_almost_equal(coe['nu'], nu_true, decimal=3)
-    assert_almost_equal(coe['u'], u_true, decimal=0)
-    assert_almost_equal(coe['lmda_true'], lmda_true_true, decimal=3)
-    assert_almost_equal(coe['what_true'], what_true_true, decimal=3)
-
-
-def test_rv2coe_to_coe2rv():
-    rIJK = np.array([6524.834, 6862.875, 6448.296])   # [km]
-    vIJK = np.array([4.901327, 5.533756, -1.976341])  # [km/s]
-    coe = pp.rv2coe(rIJK, vIJK, findall=True)
-    p = coe.get('p')
-    e = coe.get('e')
-    i = coe.get('i')
-    Omega = coe.get('Omega')
-    w = coe.get('w')
-    u = coe.get('u')
-    lmda_true = coe.get('lmda_true')
-    nu = coe.get('nu')
-    what_true = coe.get('what_true')
-    r_calc, v_calc = pp.coe2rv(p, e, i, Omega, w, nu, u, lmda_true, what_true)
-    assert_allclose(r_calc, rIJK)
-    assert_allclose(v_calc, vIJK)
-
-
-def test_coe2rv_to_rv2coe():
-    p = 11067.790   # [km]
-    e = 0.83285
-    i = 87.87       # [deg]
-    Omega = 227.89  # [deg]
-    w = 53.38       # [deg]
-    nu = 92.335     # [deg]
-    r, v = pp.coe2rv(p, e, i, Omega, w, nu)
-    coeT = pp.rv2coe(r, v, findall=True)
-    assert_almost_equal(p, coeT['p'], decimal=2)
-    assert_almost_equal(e, coeT['e'], decimal=6)
-    assert_almost_equal(i, coeT['i'], decimal=3)
-    assert_almost_equal(Omega, coeT['Omega'], decimal=3)
-    assert_almost_equal(w, coeT['w'], decimal=2)
-    assert_almost_equal(nu, coeT['nu'], decimal=3)
-
-
-# def test_coe2rv_2():
-#     """
-#     Vallado, Eg.11-6, COE from 2.4.2
-#     """
-#     a = 6768.3568   # [km]
-#     e = 0.0005770
-#     p = a*(1-e**2)
-#     i = 51.6190       # [deg]
-#     Omega = 13.3340  # [deg]
-#     w = 102.5680       # [deg]
-#     M = 257.5950  # mean anomaly
-#     E = pp.kepEqtnE(M, e)
-#     nu = pp.anomaly2nu(e, E)
-#     r_calc, v_calc = pp.coe2rv(p, e, i, Omega, w, nu)
-#     r_true = np.array([6585.038266, 1568.184321, 9.116355])  # [km]
-#     v_true = np.array([-1.1157766, 4.6316816, 6.0149576]) # [km/s]
-#     assert_allclose(r_calc, r_true, atol=1e-1, verbose=True)
-#     assert_allclose(v_calc, v_true, rtol=1e-5, verbose=True)
-
-
-# def test_pkepler():
-#     """
-#     Vallado, Eg11-6
-#     """
-#     r0 = np.array([6585.038266, 1568.184321, 9.116355])
-#     v0 = np.array([-1.1157766, 4.6316816, 6.0149576])
-#     jd0 = 2450540.400
-#     ndt = 7.889e-5
-#     # jdf = pp.julian_date(1997, 4, 2, 13, 8, 0)
-#     jdf = 2450540.5472
-#     dt = jdf - jd0
-#     rf, vf = pp.pkepler_rv(r0, v0, dt, ndt)
-#     rf_true = np.array([-2811.2769, 3486.2632, 5069.5763])
-#     vf_true = np.array([-6.859691, -2.964792, -1.764721])
-#     assert_allclose(rf, rf_true)
-#     assert_allclose(vf, vf_true)
-
-
-# def test_pkepler_2():
-#     """
-#     From Fortran test routines
-#     """
-
-#     r0 = np.array([1.02, 1.03, 1.04])
-#     v0 = np.array([0.784, 0.27, 0.012])
-#     ndt, nddt, dtsec = 0.000003, 0.00000005, 23.43
 
 
 # def test_riseset():
@@ -210,7 +91,7 @@ def test_kepEqtnE():
     e = 0.4
     E_calc = pp.kepEqtnE(M, e)
     E_true = 220.512074767522  # [deg]
-    assert_almost_equal(E_calc, E_true)
+    assert_almost_equal(E_calc, E_true, decimal=12)
 
 
 def test_julian_date():
@@ -221,7 +102,7 @@ def test_julian_date():
     hr, mn, sec = 14., 20., 0.
     jd = pp.julian_date(yr, mo, dy, hr, mn, sec)
     jdT = 2450383.09722222
-    assert_almost_equal(jd, jdT)
+    assert_almost_equal(jd, jdT, decimal=8)
 
 
 def test_julian_date_datetime():
@@ -233,7 +114,7 @@ def test_julian_date_datetime():
     dt = datetime(yr, mo, dy, hr, mn, sec)
     jd = pp.julian_date(dt)
     jdT = 2450383.09722222
-    assert_almost_equal(jd, jdT)
+    assert_almost_equal(jd, jdT, decimal=8)
 
 
 def test_julian_date_vectorized():
@@ -252,8 +133,8 @@ def test_theta_GMST1982():
     dt = datetime(1992, 8, 20, 12, 14, 0)  # Aug 20, 1992, 12:14 PM UT1
     jd = pp.julian_date(dt)
     theta, thetadt = pp.theta_GMST1982(jd)
-    theta *= 180/np.pi  # convert from radians to degrees
-    assert_almost_equal(theta, 152.578787810)  # degrees
+    theta *= pp.RAD2DEG  # convert from radians to degrees
+    assert_almost_equal(theta, 152.578787810, decimal=9)  # degrees
     # assert_almost_equal(thetadt, 152.578787810)  # degrees
 
 
@@ -264,41 +145,12 @@ def test_theta_GMST1982_2():
         Vallado, Eg. 3-15, p.230
     """
     # April 6, 2004, 07:51:28.386 009UTC, not UTC1
-    dt = datetime(2004, 4, 6, 7, 51, 28, 386009)
+    ms = int(1e6) + 386000 - 439961
+    dt = datetime(2004, 4, 6, 7, 51, 27, ms)
     jd = pp.julian_date(dt)
     theta, thetadt = pp.theta_GMST1982(jd)
-    theta *= 180/np.pi
-    assert_almost_equal(theta, 312.8098943, decimal=3)
-
-
-def test_appendix_c_conversion_from_TEME_to_ITRF():
-    """Test TEME to ITRF conversion
-
-    References:
-        Vallado et al., Revision 2
-        Rhodes, Skyfield library, test_earth_satellites.py
-    """
-    rTEME = np.array([5094.18016210, 6127.64465950, 6380.34453270])
-    vTEME = np.array([-4.746131487, 0.785818041, 5.531931288])
-    vTEME = vTEME * 24.0 * 60.0 * 60.0  # km/s to km/day
-
-    # Apr 6, 2004,  07:51:28.386 UTC
-    dt = datetime(2004, 4, 6, 7, 51, 28, 386000)
-    jd = pp.julian_date(dt)
-
-    # Polar motion
-    xp = -0.140682  # arcseconds
-    yp = 0.333309   # arcseconds
-    ARCSEC2RAD = np.pi/180 * (1/3600)
-    xp *= ARCSEC2RAD
-    yp *= ARCSEC2RAD
-    xp = yp = 0.0
-    rITRF, vITRF = pp.TEME_to_ITRF(jd, rTEME, vTEME, xp, yp)
-
-    print(rITRF)
-    assert_almost_equal(rITRF[0], -1033.47938300, decimal=1)
-    assert_almost_equal(rITRF[1], 7901.29527540, decimal=1)
-    assert_almost_equal(rITRF[2], 6380.35659580, decimal=1)
+    theta *= pp.RAD2DEG
+    assert_almost_equal(theta, 312.8098943, decimal=6)
 
 
 def test_appendix_c_conversion_from_TEME_to_ITRF_UTC1():
@@ -314,24 +166,64 @@ def test_appendix_c_conversion_from_TEME_to_ITRF_UTC1():
 
     # Apr 6, 2004,  07:51:28.386 UTC
     # deltaUTC1 = -0.439961 seconds
-    dt = datetime(2004, 4, 6, 7, 51, 28, 386000)
-    dt += timedelta(microseconds=-439961)
+    ms = int(1e6) + 386000 - 439961
+    dt = datetime(2004, 4, 6, 7, 51, 27, ms)
     jd = pp.julian_date(dt)
 
     # Polar motion
     xp = -0.140682  # arcseconds
     yp = 0.333309   # arcseconds
-    ARCSEC2RAD = np.pi/180 * (1/3600.0)
-    xp *= ARCSEC2RAD
-    yp *= ARCSEC2RAD
+    xp *= pp.ASEC2RAD
+    yp *= pp.ASEC2RAD
     # xp = yp = 0.
     rITRF, vITRF = pp.TEME_to_ITRF(jd, rTEME, vTEME, xp, yp)
 
     print(rITRF)
-    assert_almost_equal(rITRF[0], -1033.47938300, decimal=0)
-    assert_almost_equal(rITRF[1], 7901.29527540, decimal=1)
-    assert_almost_equal(rITRF[2], 6380.35659580, decimal=1)
+    assert_almost_equal(rITRF[0], -1033.47938300, decimal=4)
+    assert_almost_equal(rITRF[1], 7901.29527540, decimal=4)
+    assert_almost_equal(rITRF[2], 6380.35659580, decimal=4)
 
+
+def test_jd_from_skyfield():
+    """From skyfield.tests.test_earth_satellites.py"""
+    ms = int(1e6) + 386000 - 439961
+    dt = datetime(2004, 4, 6, 7, 51, 27, ms)
+    jd = pp.julian_date(dt)
+    assert_almost_equal(jd, 2453101.8274067827, decimal=12)
+
+
+def test_jd_from_skyfield2():
+    """From skyfield.tests.test_earth_satellites.py"""
+    ms = int(1e6) + 386000 - 439961
+    dt = datetime(2004, 4, 6, 7, 51, 27, ms)
+    jd = pp.julian_date2(dt)
+    jd_desired = 2453101.8274067827
+    print(f'jd   ={jd:20.15f}')
+    print(f'jddes={jd_desired:20.15f}')
+    print(f'diff ={jd-jd_desired:0.15f}')
+    assert_almost_equal(jd, 2453101.8274067827, decimal=12)
+
+
+def test_jd_from_skyfield3():
+    """From skyfield.tests.test_earth_satellites.py"""
+    sec = 28.386 - 0.439961
+    yr, mo, dy = 2004, 4, 6
+    hr, mn = 7, 51
+    jd = pp.julian_date2(yr, mo, dy, hr, mn, sec)
+    jd_desired = 2453101.8274067827
+    print(f'jd   ={jd:20.15f}')
+    print(f'jddes={jd_desired:20.15f}')
+    print(f'diff ={jd-jd_desired:0.15f}')
+    assert_almost_equal(jd, jd_desired, decimal=12)
+
+
+def test_thetaGMST_from_skyfield():
+    """From skyfield.tests.test_earth_satellites.py"""
+    ms = int(1e6) + 386000 - 439961
+    dt = datetime(2004, 4, 6, 7, 51, 27, ms)
+    jd = pp.julian_date(dt)
+    theta, thetadt = pp.theta_GMST1982(jd)
+    assert_almost_equal(theta, 5.459562584754709, decimal=15)
 
 # def test_sgp4():
 #     """
@@ -419,4 +311,6 @@ def test_IJK2SEZ():
 if __name__ == "__main__":
     # test_julian_date_vectorized()
     # test_theta_GMST1982()
-    test_appendix_c_conversion_from_TEME_to_ITRF_UTC1()
+    # test_appendix_c_conversion_from_TEME_to_ITRF_UTC1()
+    test_jd_from_skyfield2()
+    test_jd_from_skyfield3()
