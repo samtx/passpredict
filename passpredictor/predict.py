Get most realtime computation complete, still need to fix rotations and make accurate to heavens above

passpredictor/predict.py
@@ -84,6 +84,16 @@ def rng_el(r):
     return el, rng
 
 
+def razel(r):
+    """Get range, azimuth, and elevation from SEZ vector"""
+    rng = np.linalg.norm(r, axis=0)
+    el = np.arcsin(r[2] / rng) * RAD2DEG
+    az = (np.arctan2(r[0], r[1]) + pi * 0.5) * RAD2DEG
+    idx = np.all([r[0] < 0,r[1] < 0], axis=0)
+    az[idx] %= 360
+    return rng, az, el
+
+
 def sun_pos(t):
     """Compute the Sun position vector
 
@@ -124,7 +134,7 @@ def vector_angle(r1, r2):
     return out
 
 
-def satellite_visible(rsatECI, rsiteECEF, rho, jdt):
+def satellite_visible(rsatECI, rsiteECI, rho, jdt):
     """Determine visibility of satellite from Earth"""
     visible = np.zeros(jdt.size)
     # First, determine if satellite is above the horizon
@@ -135,14 +145,13 @@ def satellite_visible(rsatECI, rsiteECEF, rho, jdt):
         idx = vis_idx[i]
         jdt_i = jdt[idx]
         rsatECI_i = rsatECI[:, idx]
-        rsiteECEF_i = rsiteECEF[:, idx]
+        rsiteECI_i = rsiteECI[:, idx]
         rho_i = rho[:, idx]
         # Check if site is in daylight, compute dot product of sun position.
         rsun = sun_pos(jdt_i)
         if len(rsun.shape) > 1:
             rsun = rsun.flatten()
         # TODO: compute ECI vector for site position
-        rsiteECI = rsiteECEF_i
         if np.dot(rsun, rsiteECI) > 0:
             # site is in daylight
             visible[idx] = 1

passpredictor/rotations.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from .constants import (
+from passpredictor.constants import (
     R_EARTH,
     R2_EARTH,
     e_EARTH,
@@ -285,6 +285,62 @@ def precess_rotation(r, zeta, theta, z):
     )
     rGCRF[2] = -sintheta * cosz * r[0] - sintheta * sinz * r[1] + costheta * r[2]
     return rGCRF
+
+
+def TEME_to_ECEF(r, jdt):
+    """Convert TEME vectors to ECEF vectors
+
+    Args:
+        r : float (3, n) : TEME vectors
+        jdt : float (n) : julian dates
+    Returns:
+        rECEF : float (3, n): ECEF vectors
+
+    References:
+        teme2ecef.m Vallado software
+    """
+    # find GMST
+    gmst = theta_GMST1982(jdt)[0]
+
+    gmst = np.mod(gmst, tau)
+
+    costheta = np.cos(gmst)
+    sintheta = np.sin(gmst)
+    rPEF = np.empty(r.shape)
+    rPEF[0] = costheta*r[0] - sintheta*r[1]
+    rPEF[1] = sintheta*r[0] + costheta*r[1]
+    rPEF[2] = r[2]
+    rECEF = rPEF
+
+    return rECEF
+
+
+def ecef2eci(r, jdt):
+    """Convert ECEF vectors to ECI vectors
+
+    Args:
+        r : float (3, n) : ECEF vectors
+        jdt : float (n) : julian dates
+    Returns:
+        rECI : float (3, n): ECI vectors
+
+    References:
+        teme2ecef.m Vallado software
+    """
+    # find GMST
+    gmst = theta_GMST1982(jdt)
+
+    gmst = np.mod(gmst, tau)
+
+    costheta = np.cos(gmstg)
+    sintheta = np.sin(gmstg)
+    rPEF = np.empty(r.shape)
+    rPEF[0] = costheta*r[0] - sintheta*r[1]
+    rPEF[1] = sintheta*r[0] + costheta*r[1]
+    rPEF[2] = r[2]
+    rECEF = rPEF
+
+    return rECEF
 
 
 def rot1(a):

passpredictor/timefn.py
@@ -1,6 +1,6 @@
 import datetime
 import numpy as np
-from .constants import DAY_S, J2000
+from passpredictor.constants import DAY_S, J2000
 
 
 def utc2tt(UTC, deltaAT=37.0, deltaUT1=0.0):
@@ -78,3 +78,116 @@ def julian_date2(yr, mo=1, dy=1, hr=0, mn=0, sec=0.0):
         sec += dt.microsecond * (10 ** -6)
 
     return julian_day(yr, mo, dy) - 0.5 + (sec + mn * 60.0 + hr * 3600.0) / DAY_S
+
+
+def days2mdhms(year, days):
+    """This procedure converts the day of the year, days, to the equivalent
+    month, day, hour, minute and second.
+    Args:
+        year : int, year between 1900 - 2100
+        days : float, julian day of the year between 0.0 - 366.0
+    Outputs:
+        mon : int, month, 1 .. 12
+        day : int, day, 1 .. 28,29,30,31
+        hr : int, hour, 0 .. 23
+        min : int, minute 0 .. 59
+        sec : float, second, 0.0 .. 59.999
+    References:
+        Vallado
+        Rhodes, python-sgp4/sgp4/ext.py
+    """
+    # non vectorized version...
+    lmonth = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    dayofyr = int(days // 1.0)  # day of year
+    # find month and day of month
+    if (year % 4) == 0:
+        # int array containing the number of days per month
+        lmonth = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    i = 1
+    inttemp = 0
+    while dayofyr > inttemp + lmonth[i - 1] and i < 12:
+        inttemp = inttemp + lmonth[i - 1]
+        i += 1
+    mon = i
+    day = dayofyr - inttemp
+    # find hours minutes and seconds
+    temp = (days - dayofyr) * 24.0
+    hr = int(temp // 1.0)
+    temp = (temp - hr) * 60.0
+    minute = int(temp // 1.0)
+    sec = (temp - minute) * 60.0
+    return mon, day, hr, minute, sec
+
+
+def invjday(jd):
+    """This procedure finds the year, month, day, hour, minute and second given the
+    julian date. jd can be ut1, tdt, tdb, etc.
+    Args:
+        jd : float, julian date, days from 4713 BCE
+    Outputs:
+        year : int, year between 1900 - 2100
+        mon : int, month between 1 - 12
+        day : int, day between 1 - 31
+        hr : int, hour between 0 - 23
+        min : int, minute between 0 - 59
+        sec : float, second between 0.0 - 59.999
+    References:
+        Vallado, 2007, 208, alg 22, ex 3-13
+        Rhodes, python-sgp4/sgp4/ext.py
+    """
+    # find year and days of the year
+    jd = np.atleast_1d(jd)
+    temp = jd - 2415019.5
+    tu = temp / 365.25  # julian centuries from 0 h jan 0, 1900
+    year = 1900 + np.floor_divide(tu, 1.0).astype(int)
+    leapyrs = np.floor_divide(((year - 1901) * 0.25), 1.0).astype(int)  # number of leap years from 1900
+    # optional nudge by 8.64x10-7 sec to get even outputs
+    # day of year plus fractional portion of a day
+    days = temp - ((year - 1900) * 365.0 + leapyrs) + 0.00000000001
+    # check for case of beginning of a year
+    day1_idx = days < 1.0
+    year[day1_idx] = year[day1_idx] - 1
+    leapyrs[day1_idx] = np.floor_divide((year[day1_idx] - 1901) * 0.25, 1.0).astype(int)
+    days[day1_idx] = temp[day1_idx] - ((year[day1_idx] - 1900) * 365.0 + leapyrs[day1_idx])
+    # find remaing data
+    jd_size = len(jd)
+    mon = np.empty(jd_size)
+    day = np.empty(jd_size)
+    hr = np.empty(jd_size)
+    minute = np.empty(jd_size)
+    sec = np.empty(jd_size)
+    for i in range(jd_size):
+        mon[i], day[i], hr[i], minute[i], sec[i] = days2mdhms(year[i], days[i])
+    sec = sec - 0.00000086400
+    return year, mon, day, hr, minute, sec
+
+
+def invjday_to_npdatetime64(year, mon, day, hr, minute, sec):
+    """Take arrays of datetime values and return array of datetime objects
+
+    Args:
+
+    Returns:
+        dtary : np.datetime64 (n)
+    """
+    n = len(year)
+    s = np.floor(sec).astype(int)
+    us = (np.mod(sec, 1) * 1e6).astype(int)
+    dtary = np.empty(n, dtype=object)
+    for i in range(len(year)):
+        # dtstr = f'{year[i]:4d}-{int(mon[i]):02d}-{int(day[i]):02d}T{int(hr[i]):02d}:{int(minute[i]):02d}:{sec[i]:05.2f}'
+        dtary[i] = datetime.datetime(year[i], int(mon[i]), int(day[i]), int(hr[i]), int(minute[i]), s[i], us[i])
+    return dtary
+
+def jdt_tsince(tstart, tsince):
+    """Return a vector of julian dates from tstart with points at tsince
+    Args:
+        tstart : float, julian date
+        tsince: float (n), vector of minutes past tstart to calculate the julian date
+    Output:
+        jdt : float (n), vector of julian date ouputs. Can be inputted into solar functions
+    References:
+        Rhodes, python-sgp4/sgp4/ext.py
+        Vallado, 'Revisiting Spacetrack Report #3'
+    """
+    return tstart + (tsince * 60.0) / 86400.0