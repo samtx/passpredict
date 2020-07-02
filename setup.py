d928d9bea34acc5a391fdb287aebc94f323a9ff3
Sam Friedman <samtx@outlook.com>
2020-07-02 10:34:27 -0500

    Add requirements-dev.txt for pytest and flake8

--- setup.py
+++ setup.py
@@ -15,9 +15,7 @@ setup(
             'docs', 'tests'
         ]
     ),
-
     python_requires=">=3.7", 
-
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
     install_requires=[
@@ -27,14 +25,19 @@ setup(
         "pydantic",
         "click",
     ],
-    # setup_requires=[
-    #     "cython",
-    # ]
+    extras_require={
+        'dev': [
+            'Cython',
+            'pytest',
+            'pytest-html',
+            'pytest-cov',
+            'flake8',
+        ]
+    }
     package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
+        # If any package contains *.txt, *.rst, *.dat, *.csv files, include them:
+        '': ['*.txt', '*.rst', '*.dat', '*.csv'],
     },
-
     # metadata to display on PyPI
     author="Sam Friedman",
     author_email="samtx@outlook.com",
