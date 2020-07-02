7aa92c194cf0416e5e5bffc6d3d6510e77678187
Sam Friedman <samtx@outlook.com>
2020-07-02 11:29:31 -0500

    Add nutation, precession, rotation functions

--- tests/test_precession.py
+++ tests/test_precession.py
@@ -6,6 +6,8 @@ import pytest
 
 import passpredict.precession as precession 
 import passpredict.constants as constants
+from passpredict.utils import epoch_from_tle_datetime
+from passpredict.timefn import julian_date, jd2jc
 
 
 
@@ -21,18 +23,44 @@ def test_fk5_precession_angles():
     assert_almost_equal(z, 0.0273059 * constants.DEG2RAD, decimal=9)
 
 
-def test_fk5_precession():
+def test_fk5_precession_angles_2():
     """
-    Vallado, Eg. 3-15, p.231
+    Vallado, p.234
     """
-    tt = 0.0426236319
-    rMOD = np.array([5094.0283745, 6127.8708164, 6380.2485164])
-    # zeta = 0.0273055 * constants.DEG2RAD
-    # theta = 0.0237306 * constants.DEG2RAD
-    # z = 0.0273059 * constants.DEG2RAD
-    prec = precession.fk5_precession(tt)
-    rGCRF_true = np.array([5102.508958, 6123.011401, 6378.136928])
-    assert_allclose(rGCRF, rGCRF_true)
+    epoch = epoch_from_tle_datetime('00182.78495062')
+    jd = julian_date(epoch)
+    tt = jd2jc(jd)
+    zeta, theta, z = precession.fk5_precession_angles(tt)
+    assert_almost_equal(zeta, 0.0031796 * constants.DEG2RAD, decimal=9)
+    assert_almost_equal(theta, 0.0027633 * constants.DEG2RAD, decimal=9)
+    assert_almost_equal(z, 0.0031796 * constants.DEG2RAD, decimal=9)
+
+
+def test_fk5_precession_angles_3():
+    """
+    Vallado, p.234
+    """
+    epoch = epoch_from_tle_datetime('00179.78495062')
+    jd = julian_date(epoch)
+    tt = jd2jc(jd)
+    zeta, theta, z = precession.fk5_precession_angles(tt)
+    assert_almost_equal(zeta, 0.0031270 * constants.DEG2RAD, decimal=9)
+    assert_almost_equal(theta, 0.0027176 * constants.DEG2RAD, decimal=9)
+    assert_almost_equal(z, 0.0031270 * constants.DEG2RAD, decimal=9)
+
+
+# def test_fk5_precession():
+#     """
+#     Vallado, Eg. 3-15, p.231
+#     """
+#     tt = 0.0426236319
+#     rMOD = np.array([5094.0283745, 6127.8708164, 6380.2485164])
+#     # zeta = 0.0273055 * constants.DEG2RAD
+#     # theta = 0.0237306 * constants.DEG2RAD
+#     # z = 0.0273059 * constants.DEG2RAD
+#     prec = precession.fk5_precession(tt)
+#     rGCRF_true = np.array([5102.508958, 6123.011401, 6378.136928])
+#     assert_allclose(r, rGCRF_true)
 
 
 
