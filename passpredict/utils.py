2057a7a846d378bec69069ed17ba5667db378bb1
Sam Friedman <samtx@outlook.com>
2021-07-17 11:30:02 -0500

    move cache classes to new cache.py file

--- passpredict/utils.py
+++ passpredict/utils.py
@@ -1,11 +1,6 @@
 from itertools import zip_longest
-import shelve
-import time
-import json
-from dataclasses import dataclass, asdict
-from typing import Any
 
-import numpy as np
+from numpy import pi as np_pi
 
 
 def shift_angle(x: float) -> float:
@@ -17,7 +12,7 @@ def shift_angle(x: float) -> float:
     Reference: 
         https://stackoverflow.com/questions/15927755/opposite-of-numpy-unwrap/32266181#32266181
     """
-    return (x + np.pi) % (2 * np.pi) - np.pi
+    return (x + np_pi) % (2 * np_pi) - np_pi
 
 
 def grouper(iterable, n, fillvalue=None):
@@ -27,105 +22,3 @@ def grouper(iterable, n, fillvalue=None):
     """
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
-
-
-class JsonCache:
-    """
-    A cache for downloaded TLE data using a json file
-    """
-    filename = 'tle.json'
-
-    def __init__(self, filename=filename):
-        self.filename = filename
-        self.cache = {}
-
-    def __contains__(self, key):
-        return key in self.cache
-
-    def __setitem__(self, key, value):
-        self.cache[key] = value
-
-    def __getitem__(self, key):
-        return self.cache[key]
-
-    def __delitem__(self, key):
-        del self.cache[key]
-
-    def __enter__(self):
-        self.cache = json.load(self.filename)
-        
-    def __exit__(self, *args):
-        json.dump(self.cache, self.filename)
-
-    def get(self, key, ignore_ttl=False):
-        if key not in self: return None
-        item = self.cache[key]
-        if (not ignore_ttl) and (ttl := item['ttl']):
-            if time.time() > ttl:
-                del self.cache[key]
-                return None
-        return item['data']
-
-    def set(self, key, value, ttl=None):
-        item = {'ttl': ttl, 'data': value}
-        self[key] = item
-
-
-class ShelfCache:
-    """
-    A cache for downloaded TLE data using a shelf.
-    """
-    filename = 'tle.db'
-        
-    def __init__(self, filename=filename, ttl=86400):   
-        self.filename = filename
-        self.ttl = ttl
-        self.cache = {}
-
-    def set(self, key, value):
-        key_hash = self._hash(key)
-        self.cache[key_hash] = value
-            
-    def get(self, key, *a):
-        key_hash = self._hash(key)
-        value = self.cache.get(key_hash, *a)
-        return value
-
-    def pop(self, key, default_value=None, index=None):
-        key_hash = self._hash(key)
-        if key_hash in self.cache:
-            value = self.get(key)
-            del self.cache[key_hash]
-            return value
-        else:
-            return default_value        
-
-    def __contains__(self, key):
-        key_hash = self._hash(key)
-        return key_hash in self.cache
-
-    def __setitem__(self, key, value):
-        self.set(key, value)
-
-    def __getitem__(self, key):
-        return self.get(key)
-
-    def __delitem__(self, key):
-        key_hash = self._hash(key)
-        del self.cache[key_hash]
-
-    def _hash(self, key):
-        return str(key)
-
-    def __enter__(self):
-        return self.open()
-
-    def __exit__(self, *a):
-        self.close()
-
-    def open(self):
-        self.cache = shelve.DbfilenameShelf(self.filename)
-        return self
-        
-    def close(self):
-        self.cache.close()
