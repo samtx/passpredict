62e70317479d1c230fec0be4491d67696726e62b
Sam Friedman <samtx@outlook.com>
2021-07-17 12:00:26 -0500

    remove extra input variables from predict() function

--- passpredict/predictions.py
+++ passpredict/predictions.py
@@ -37,70 +37,25 @@ def find_overpasses(
 def predict(
     location: Location,
     satellite: Satellite,
-    date_start: datetime = None,
-    date_end: datetime = None,
-    dt_seconds: int = 1,
+    date_start: datetime,
+    date_end: datetime,
     min_elevation: float = 10.0,
-    cache: 'Cache' = None,
-    verbose: bool = False, 
-    store_sat_id: bool = False, 
-    print_fn: Callable = print, 
-    visible_only: bool = False
 ):
     """
     Full prediction algorithm:
-      1. Download TLE data
-      2. Propagate satellite using SGP4
-      3. Predict overpasses based on site location
-      4. Return overpass object and print to screen
-
+        - Propagate satellite using SGP4
+        - Predict overpasses based on site location
+    
     Params:
         location : Location object
             latitude of site location, in decimal, north is positive
         satellite: Satellite object
             satellite ID number in Celestrak, ISS is 25544
     """
-    if date_start is None:
-        date_start = datetime.date.today()
-    if date_end is None:
-        date_end = date_start + timedelta(days=14)
-
-    jd = julian_date_array_from_date(date_start, date_end, dt_seconds)
-
-    if cache is None:
-        t = Time(jd, format='jd')
-        sun = compute_sun_data(t)
-        tle = get_TLE(satellite.id)
-        sat = compute_satellite_data(tle, t, sun)
-
-    else:
-
-        with cache:
-
-            time_key = 'time:' + date_start.strftime('%Y%m%d') + date_end.strftime('%Y%m%d') + str(dt_seconds)
-
-            sun_key = 'sun:' + time_key
-            sun = cache.get(sun_key)
-            if sun is None:
-                if verbose:
-                    print_fn("Compute sun position... ")
-                t = Time(jd, format='jd')
-                sun = compute_sun_data(t)
-                cache.set(sun_key, sun, ttl=86400)
-
-            sat_key = 'sat:' + str(satellite.id) + time_key
-            sat = cache.get(sat_key)
-            if sat is None:
-                if verbose:
-                    print_fn(f"Propagate satellite from {date_start.isoformat()} to {date_end.isoformat()}... ")
-                tle = get_TLE(satellite.id)
-                t = Time(jd, format='jd')
-                sat = compute_satellite_data(tle, t, sun)
-                cache.set(sat_key, sat, ttl=86400)
-
-    if verbose:
-        print_fn('Predict overpasses... ')
-    overpasses = find_overpasses(jd, location, [sat], sun, min_elevation, visible_only=visible_only)
+    t = Time(jd, format='jd')
+    sun = compute_sun_data(t)
+    tle = get_TLE(satellite.id)
+    sat = compute_satellite_data(tle, t, sun)
     return overpasses
 
 
