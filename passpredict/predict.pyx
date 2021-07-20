09ca71db665ce91acb919192dc1729e53e45b20a
Sam Friedman <samtx@outlook.com>
2021-07-20 15:09:48 -0500

    Create cython timefn for wrapping TLE epoch to julian date cpp functions from sgp4lib

--- passpredict/predict.pyx
+++ passpredict/predict.pyx
@@ -7,7 +7,7 @@ cimport numpy as np
 
 ctypedef np.float64_t DTYPE_f64_t
 
-cdef extern from "passpredict.hpp":
+cdef extern from "ext/passpredict.hpp":
     void c_sun_pos(double *jd, double *r, int n)
     void c_sun_pos_ecef(double *jd, double *r, int n)
     void c_sun_sat_illumination_distance(double *rsat, double *rsun, double *illum_dist, int n)
