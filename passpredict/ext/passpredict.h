001826e647043c7eceb23996da5b798f3b1f1a81
Sam Friedman <samtx@outlook.com>
2021-07-22 09:58:49 -0500

    make satrec struct from Omm data

--- passpredict/ext/passpredict.h
+++ passpredict/ext/passpredict.h
@@ -3,6 +3,15 @@
 
 #include "sofa.h"
 
+#define PASSPREDICT_R_EARTH 6378.137
+#define PASSPREDICT_DEG2RAD 1.745329251994329576923691e-2  // sofam.m DD2R
+#define PASSPREDICT_2PI 6.283185307179586476925287      // sofam.m D2PI
+
+
+namespace passpredict {
+
+}
+
 #define D_R_EARTH 6378.137  //  Earth mean equatorial radius [km]
 
 /*  crotations.c  */
