9f593c61adf6368b6991c6a2c4335eb7449ba36e
Sam Friedman <samtx@outlook.com>
2021-07-23 23:17:49 -0500

    Add satellite propagation and coordinate rotation test cases

--- passpredict/ext/satellite.cpp
+++ passpredict/ext/satellite.cpp
@@ -13,7 +13,11 @@ namespace passpredict
     Satellite::Satellite(Orbit orbit)
     {
         this->orbit = orbit;
-        m_epoch = orbit.satrec.jdsatepoch + orbit.satrec.jdsatepochF;
+        epoch = orbit.satrec.jdsatepoch + orbit.satrec.jdsatepochF;
+    };
+
+    void Satellite::sgp4() {
+        SGP4Funcs::sgp4(orbit.satrec, tsince, rteme, vteme);
     };
 
     void Satellite::propagate_tsince(double t_tsince)
@@ -23,10 +27,10 @@ namespace passpredict
                 t_tsince    time since epoch in minutes
             */
         // find time since julian date in minutes
-        m_jd = m_epoch + (t_tsince / 1440.0);
-        m_tsince = t_tsince;
-        SGP4Funcs::sgp4(orbit.satrec, m_tsince, m_rteme, m_vteme);
-
+        tsince = t_tsince;
+        jd = epoch + (t_tsince / 1440.0);
+        Satellite::sgp4();
+        Satellite::teme2ecef();
     };
 
     void Satellite::propagate_jd(double t_jd)
@@ -36,9 +40,10 @@ namespace passpredict
                 t_jd    julian date
             */
         // find time of julian date since epoch in minutes
-        m_jd = t_jd;
-        m_tsince = (m_epoch - m_jd) / 1440.0;
-        SGP4Funcs::sgp4(orbit.satrec, m_tsince, m_rteme, m_vteme);
+        jd = t_jd;
+        tsince = (epoch - jd) / 1440.0;
+        Satellite::sgp4();
+        Satellite::teme2ecef();
     };
 
     int Satellite::teme2ecef()
@@ -54,11 +59,11 @@ namespace passpredict
         double rotz[3][3], cosw, sinw, tmp[3] = {0, 0, 0};
 
         // find GMST
-        gmst = iauGmst82(m_jd, 0.0);
+        gmst = iauGmst82(jd, 0.0);
 
         // Find terrestial time
         // Convert UTC to TAI
-        err = iauUtctai(m_jd, 0.0, &tai_1, &tai_2);
+        err = iauUtctai(jd, 0.0, &tai_1, &tai_2);
         if (err) return 1;
         // Convert TAI to TT
         err = iauTaitt(tai_1, tai_2, &tt_1, &tt_2);
@@ -89,9 +94,9 @@ namespace passpredict
         cosw = std::cos(w);
         sinw = std::sin(w);
         rotz[0][0] = cosw;
-        rotz[0][1] = -sinw;
+        rotz[0][1] = sinw;
         rotz[0][2] = 0;
-        rotz[1][0] = sinw;
+        rotz[1][0] = -sinw;
         rotz[1][1] = cosw;
         rotz[1][2] = 0;
         rotz[2][0] = 0;
@@ -103,7 +108,7 @@ namespace passpredict
         for (i=0; i<3; i++) {
             recef[i] = 0.0;
             for (j=0; j<3; j++) {
-                recef[i] += rotz[i][j] * m_rteme[j];
+                recef[i] += rotz[i][j] * rteme[j];
             }
         }
     }
@@ -113,17 +118,17 @@ namespace passpredict
         int i;
         using namespace std;
         cout.precision(10);
-        cout << "jd    =  " << fixed << m_jd << endl;
+        cout << "jd    =  " << fixed << jd << endl;
         cout.precision(4);
         cout << "rteme = [" << fixed;
         for (i = 0; i < 3; i++)
         {
-            cout << m_rteme[i] << ", ";
+            cout << rteme[i] << ", ";
         }
         cout << "]" << endl;
         cout << "vteme = [" << fixed;
         for (i = 0; i < 3; i++)
-            cout << m_vteme[i] << ", ";
+            cout << vteme[i] << ", ";
         cout << "]" << endl;
     };
 
@@ -132,10 +137,10 @@ namespace passpredict
         int i;
         using namespace std;
         cout.precision(8);
-        cout << "   " << setw(14) << m_tsince;
+        cout << "   " << setw(14) << tsince;
         for (i = 0; i < 3; i++)
-            cout << "   " << setw(14) << m_rteme[i];
+            cout << "   " << setw(14) << rteme[i];
         for (i = 0; i < 3; i++)
-            cout << "   " << setw(12) << m_vteme[i];
+            cout << "   " << setw(12) << vteme[i];
     }
 }
