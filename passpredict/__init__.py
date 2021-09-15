30906c95dfa937c8a7c5ecf3e6ac8ee6f5341188
Sam Friedman <samtx@outlook.com>
2021-09-15 12:30:35 -0500

    Move Location and Satellite objects from schemas.py to predict.pyx to be Cython cdef classes
    Create cython test file to make sure that compilation and object init works
    
    Modify setup.py to create two extension modules, one for time functions and one for predict classes and functions

--- passpredict/__init__.py
+++ passpredict/__init__.py
@@ -1,6 +1,2 @@
-try:
-    from .predict import Location, Point
-except ImportError:
-    raise ImportError('Must compile with Cython first')
-
 from .tle import OMM
+from .predict import Location, Satellite
