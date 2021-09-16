1c40d1c3e464f56c3b59bd231fd7b3e97763d861
Sam Friedman <samtx@outlook.com>
2021-09-15 20:50:53 -0500

    Create Observer python object to store location and satellite objects

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import time
+import datetime
 
 from passpredict import Location, Satellite, OMM
 from passpredict.predict import compute_elevation_angle
+from passpredict import predict_py
 
-def main():
+# class CentralTime(datetime.tzinfo):
+#     def __init__(self):
+#         super().__init__(offset=datetime.timedelta(hours=-6))
+
+
+def benchmark_compute_elevation_angle():
     tle1 = '1 25544U 98067A   20166.98401036  .00000505  00000-0  17092-4 0  9999'
     tle2 = '2 25544  51.6466 359.3724 0002481  58.1246  97.0831 15.49444148231675'
     omm = OMM.from_tle(tle1, tle2)
@@ -20,5 +27,26 @@ def main():
     print(f"Time: {(t2-t1)/n*1e6:f} us")
 
 
+def predict_algorithm():
+    """
+    Test out main prediction algorithm
+    """
+    tle1 = '1 25544U 98067A   21258.74288194 -.00052450  00000-0 -97172-3 0  9998'
+    tle2 = '2 25544  51.6428 254.9623 0002916  17.0077 340.1623 15.48363749302621'
+    omm = OMM.from_tle(tle1, tle2)
+    satellite = Satellite(omm)
+    location = Location(30.1957, -97.8649, 0)
+    observer = predict_py.Observer(location, satellite)
+    t1 = time.perf_counter()
+    res = observer.predict_overpasses()
+    t2 = time.perf_counter()
+    print(f'Time: {(t2-t1)*1000:f} ms')
+    offset = datetime.timedelta(hours=-5)
+    datetime_local = [d.replace(tzinfo=None) + offset for d in res]
+    from pprint import pprint
+    pprint(datetime_local)
+
+
 if __name__ == "__main__":
-    main()
+    predict_algorithm()
+    # benchmark_compute_elevation_angle()
