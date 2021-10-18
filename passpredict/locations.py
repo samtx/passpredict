02574f5305183508c7af7d4496182c37c8587053
Sam Friedman <samtx@outlook.com>
2021-10-17 21:53:20 -0500

    Update JsonCache to use Tle objects from Orbit Predictor
    Update github action to build wheels for linux, windows, mac with python 3.8 and 3.9
    
    Replace requests with httpx

--- passpredict/locations.py
+++ passpredict/locations.py
@@ -3,6 +3,18 @@ from orbit_predictor.locations import Location as LocationBase
 
 class Location(LocationBase):
 
+    @property
+    def lat(self):
+        return self.latitude_deg
+
+    @property
+    def lon(self):
+        return self.longitude_deg
+
+    @property
+    def h(self):
+        return self.elevation_m
+
     def __repr__(self):
         s = '<Location '
         if self.name:
